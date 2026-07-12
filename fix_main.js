d6d7f1dc7be331ae687fbe132c06626d4a0bb71e
apelabsnft <tejus.sharma10@gmail.com>
2026-07-13 01:07:50 +0530

    Fix encodeURIComponent breaking DCLogic parsing

--- fix_main.js
+++ fix_main.js
@@ -1,12 +1,25 @@
 const fs = require('fs');
-let c = fs.readFileSync('Home.dc.html', 'utf8');
-const search = '<main id="main-content">\n<main id="main-content">';
-const search2 = '<main id="main-content">\r\n<main id="main-content">';
-if (c.includes(search)) {
-  c = c.replace(search, '<main id="main-content">');
+const index = fs.readFileSync('index.html', 'utf8');
+const home = fs.readFileSync('Home.dc.html', 'utf8');
+
+const indexHead = index.substring(0, index.indexOf('<x-dc>'));
+const indexTailStart = index.indexOf('<script defer src="/site-analytics.min.js">');
+let indexTail = '';
+if (indexTailStart !== -1) {
+    indexTail = index.substring(indexTailStart);
+} else {
+    indexTail = '<script defer src="/site-analytics.min.js"></script>\n<script defer src="/google-reviews.min.js"></script>\n<script defer src="/site-i18n.min.js"></script>\n</body>\n</html>';
 }
-if (c.includes(search2)) {
-  c = c.replace(search2, '<main id="main-content">');
+
+const homeBodyStart = home.indexOf('<x-dc>');
+const homeBodyEnd = home.indexOf('</script>\n\n<script defer src="/site-analytics.min.js">') + '</script>\n'.length;
+
+let homeBody = home.substring(homeBodyStart);
+// if home has the analytics scripts, strip them off
+const homeTail = homeBody.indexOf('\n<script defer src="/site-analytics.min.js">');
+if (homeTail !== -1) {
+    homeBody = homeBody.substring(0, homeTail);
 }
-fs.writeFileSync('Home.dc.html', c);
-fs.writeFileSync('index.html', c);
+
+fs.writeFileSync('index.html', indexHead + homeBody + '\n' + indexTail);
+console.log('Done merging Home.dc.html into index.html');
